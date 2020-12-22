1f0d37929658bc679561f9c45cd916841cbac3bc
19jz0137 <19jz0137@R1241PC22.jz.jec.ac.jp>
2020-12-22 15:22:41 +0900

    CustomerDAO作成途中

--- src/employeesMenu/customer/CustomerDAO.java
+++ src/employeesMenu/customer/CustomerDAO.java
@@ -8,6 +8,8 @@ package employeesMenu.customer;
 import sys.DBManager;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * 顧客DAO
@@ -24,5 +26,51 @@ public class CustomerDAO {
     public CustomerDAO(){
         DBManager dbManager = DBManager.getDBManager();
         con = dbManager.getConnection();
+    }
+    
+    public Customer selectCustomerExcute() throws SQLException {
+        try {
+            ResultSet rs = ps.executeQuery();
+            setCustomer(customer, rs);
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * 問い合わせ結果をcustomerに格納
+     * @param customer 問い合わせ結果を格納
+     * @param rs       問い合わせ結果
+     */
+    public void setCustomer(Customer customer, ResultSet rs) {
+        try {
+            String name = rs.getString("NAME");
+            String phoneNumber = rs.getString("PHONE_NUMBER");
+            String address = rs.getString("ADDRESS");
+            String deliveryNote = rs.getString("DELIVERY_NOTE");
+            customer.setName(name);
+            customer.setPhoneNumber(phoneNumber);
+            customer.setAddress(address);
+            customer.setDeliveryNote(deliveryNote);
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        
+    }
+            
+    
+    public Customer dbSearchCustomerPhoneNumber(String phoneNumber) {
+        String sql = "SELECT * FROM CUSTOMERS " + 
+                     " WHERE PHONE_NUMBER = ? ";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, phoneNumber);
+            return 
+        }
+        catch (SQLException e) {
+        }
     }
 }
