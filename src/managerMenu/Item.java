ee000c5e9030879f4d7891084cf9c92468497d57
19jz0137 <19jz0137@R1241PC22.jz.jec.ac.jp>
2021-01-14 16:34:44 +0900

    商品情報クラス作成

---
+++ src/managerMenu/Item.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managerMenu;
+
+/**
+ * 商品クラス
+ * @author 19jz0137
+ */
+public class Item {
+    private String itemNumber;
+    private String itemName;
+    private int    unitPrice;
+
+    public Item() {
+    }
+
+    public Item(String itemNumber, String itemName, int unitPrice) {
+        this.itemNumber = itemNumber;
+        this.itemName = itemName;
+        this.unitPrice = unitPrice;
+    }
+
+    public String getItemNumber() {
+        return itemNumber;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setItemNumber(String itemNumber) {
+        this.itemNumber = itemNumber;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public void setUnitPrice(int unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+    
+    
+}
