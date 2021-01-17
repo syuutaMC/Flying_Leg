e9842b07f98e0d96955c416d09ce305571d7e4e9
tomo_chan <tomoki.mlqll@gmail.com>
2021-01-17 23:23:14 +0900

    注文明細表にもデータが挿入されるように変更

--- src/employeesMenu/order/OrderDAO.java
+++ src/employeesMenu/order/OrderDAO.java
@@ -8,6 +8,7 @@ package employeesMenu.order;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import managerMenu.Item;
 import sys.DBManager;
 
 /**
@@ -31,18 +32,31 @@ public class OrderDAO {
      * 注文追加処理
      * @param customerNumber    顧客番号
      * @param deliveryToAddress 配達先住所
+     * @param items              注文商品
      */
-    public void dbAddOrder(int customerNumber, String deliveryToAddress) throws SQLException  {
-        String sql = "INSERT INTO " + 
-                     "ORDERS(customer_number, delivery_to_address, store_number) " + 
-                     "VALUES(?, ?, ?) ";
+    public void dbAddOrder(int customerNumber, String deliveryToAddress, Item[] items) throws SQLException  {
+        String sql1 = "INSERT INTO " + 
+                     " ORDERS(customer_number, delivery_to_address, store_number) " + 
+                     " VALUES(?, ?, ?) ";
+        
+        String sql2 = "INSERT INTO " +
+                      " ORDER_DETAILS(order_number, item_number, order_quantity)" +
+                      " VALUES( orders_order_no_sq, ?, ?)";
         try {
-            ps = con.prepareStatement(sql);
+            ps = con.prepareStatement(sql1);
             ps.setInt(1, customerNumber);
             ps.setString(2, deliveryToAddress);
-            ps.setString(3, "001");
+            ps.setString(3, "001"); //店舗番号
             
             ps.executeUpdate();
+            
+            ps = con.prepareStatement(sql2);
+            for (Item item : items) {
+                ps.setString( 1, item.getItemNumber());
+                ps.setInt(2, item.getQuantity());
+                
+                ps.executeUpdate();
+            }
         }
         catch (SQLException e) {
             throw e;
