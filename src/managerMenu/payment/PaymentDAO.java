b06f23a57ff8493398167f8019ecee567fdeda06
tomo_chan <tomoki.mlqll@gmail.com>
2021-02-03 23:25:43 +0900

    支払情報DAO全件検索処理追加

--- src/managerMenu/payment/PaymentDAO.java
+++ src/managerMenu/payment/PaymentDAO.java
@@ -7,6 +7,11 @@ package managerMenu.payment;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import sys.DBManager;
 
 /**
@@ -20,6 +25,65 @@ public class PaymentDAO {
     public PaymentDAO() {
         DBManager dBManager = DBManager.getDBManager();
         con = dBManager.getConnection();
+    }
+    
+    public List<Payment> selectPaymentExecute() throws SQLException {
+        List<Payment> paymentList = new ArrayList<>();
+        try {
+            paymentList.clear();
+            ResultSet rs = ps.executeQuery();
+            //結果の格納
+            while (rs.next()) {                
+                Payment payment = new Payment();
+                setPayment(payment, rs);
+                paymentList.add(payment);
+            }
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return paymentList;
+    }
+    
+    /**
+     * 問い合わせ結果をpaymentに格納
+     * @param payment 支払情報
+     * @param rs      問い合わせ結果
+     */
+    public void setPayment(Payment payment, ResultSet rs) {
+        try {
+            String  name        = rs.getString("NAME");
+            String  phoneNumber = rs.getString("PHONE_NUMBER");
+            Date    orderDate   = rs.getDate("ORDER_DATE");
+            Date    paymentDay  = rs.getDate("PAYMENT_DAY");
+            int     amount      = rs.getInt("AMOUNT");
+            payment.setName(name);
+            payment.setPhoneNumber(phoneNumber);
+            payment.setOrderDate(orderDate);
+            payment.setPaymentDay(paymentDay);
+            payment.setAmount(amount);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * 支払情報を全件問い合わせ（結果は注文日の降順）
+     * @return 支払情報
+     * @throws SQLException 
+     */
+    public List<Payment> dbSearchPaymentAll() throws SQLException{
+        List<Payment> paymentList = new ArrayList<>();
+        String sql = "SELECT * FROM payments";
+        try {
+            ps = con.prepareStatement(sql);
+            paymentList = selectPaymentExecute();
+        } catch (SQLException e) {
+            throw e;
+        }
+        
+        return paymentList;
     }
     
     
