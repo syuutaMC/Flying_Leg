88530e9e57d063245575cb788e98d3ede29fde06
tomo_chan <tomoki.mlqll@gmail.com>
2021-02-13 02:49:49 +0900

    salesAtWeek検索処理を追加

--- src/managerMenu/payment/SalesDAO.java
+++ src/managerMenu/payment/SalesDAO.java
@@ -12,6 +12,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import javax.crypto.SealedObject;
 import sys.DBManager;
 
 /**
@@ -58,7 +59,7 @@ public class SalesDAO {
      */
     public void setPayment(Sales sales, ResultSet rs) {
         try {
-            Date salesDate     = rs.getDate("SALES_DATE");
+            Date salesDate      = rs.getDate("SALES_DATE");
             int  storeNumber    = rs.getInt("STORE_NUMBER");
             int  orderQuantity  = rs.getInt("ORDER_QUANTITY");
             int  salesAmount    = rs.getInt("SALES_AMOUNT");
@@ -72,7 +73,57 @@ public class SalesDAO {
             e.printStackTrace();
         }
     }
-      
+    
+    public List<SalesAtWeek> selectSalesAtWeeksExecute() throws SQLException {
+        List<SalesAtWeek> salesAtWeekList = new ArrayList<>();
+        try {
+            salesAtWeekList.clear();
+            ResultSet rs = ps.executeQuery();
+            //結果の格納
+            while (rs.next()) {                
+                SalesAtWeek salesAtWeek = new SalesAtWeek();
+                setSalesAtWeek(salesAtWeek, rs);
+                salesAtWeekList.add(salesAtWeek);
+            }
+            rs.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return salesAtWeekList;
+    }
+    
+    /**
+     * 問い合わせ結果をsalesAtWeekに格納
+     * @param salesAtWeek 曜日売上
+     * @param rs 　　　　　問い合わせ結果
+     */
+    public void setSalesAtWeek(SalesAtWeek salesAtWeek, ResultSet rs) {
+        try {
+            Date salesDate      = rs.getDate("SALES_DATE");
+            int  storeNumber    = rs.getInt("STORE_NUMBER");
+            int  sun            = rs.getInt("SUN");
+            int  mon            = rs.getInt("MON");
+            int  tue            = rs.getInt("TUE");
+            int  wed            = rs.getInt("WED");
+            int  thu            = rs.getInt("THU");
+            int  fry            = rs.getInt("FRY");
+            int  sat            = rs.getInt("SAT");
+            
+            salesAtWeek.setSalesDate(salesDate);
+            salesAtWeek.setStoreNumber(storeNumber);
+            salesAtWeek.setSun(sun);
+            salesAtWeek.setMon(mon);
+            salesAtWeek.setYue(tue);
+            salesAtWeek.setWed(wed);
+            salesAtWeek.setThu(thu);
+            salesAtWeek.setFry(fry);
+            salesAtWeek.setSat(sat);
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
     /**
      * 当日の売上を検索する
      * @return 当日の売り上げ
