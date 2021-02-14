5a30239666d0b45e775163a8ef63126215053cc8
tomo_chan <tomoki.mlqll@gmail.com>
2021-02-15 04:40:03 +0900

    コンボボックス初期化処理作成

--- src/managerMenu/item/ManageItemControl.java
+++ src/managerMenu/item/ManageItemControl.java
@@ -5,20 +5,37 @@
  */
 package managerMenu.item;
 
+import java.sql.SQLException;
+import java.util.List;
 import sys.SystemControl;
 /**
  *
  * @author syuuta
  */
 public class ManageItemControl {
-    private ManageItemBoundary manageItemBoundary;
+    private final ManageItemBoundary manageItemBoundary;
+    private final CategoryDAO categoryDAO;
     private SystemControl control;
     
     public ManageItemControl(){
         manageItemBoundary = new ManageItemBoundary();
+        categoryDAO = new CategoryDAO();
     }
     
     public void setControl(SystemControl control){
         this.control = control;
+    }
+    
+    /**
+     * コンボボックスに商品カテゴリを設定
+     */
+    public void setCategoryComboBox() {
+        List<Category> categoryList;
+        try {
+            categoryList = categoryDAO.dbSearchItemCategoryAll();
+            manageItemBoundary.setCategoryComboBox(categoryList);
+        } catch (SQLException e) {
+            manageItemBoundary.showDBErrorMessage();
+        }
     }
 }
